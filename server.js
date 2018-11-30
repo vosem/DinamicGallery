b48e9d5363ebaca8d801f3ad5f96cdd36358421d
vosem <trefoil2@ukr.net>
2018-11-30 15:08:14 +0200

    'fix'

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const http = require('http')
+const http = require('http');
 const path = require('path');
 
 const app = express();
@@ -10,7 +10,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + 'dist/index.html'));
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.set('port', port);
 
 const server = http.createServer(app);
