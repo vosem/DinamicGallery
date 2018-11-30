d7cd785b2b086b70f48e5a6c6fdd7ac1133d50f5
vosem <trefoil2@ukr.net>
2018-11-30 14:56:40 +0200

    'fixing'

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const http = require('http')
 const path = require('path');
+
 const app = express();
 
-app.use(express.static(__dirname = '/dist/dinamic-gallery'));
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/*', function (req, res) {
-	res.sendFile(path.join(__dirname+'/dist/dinamic-gallery/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
-app.listen(process.env.PORT || 8080);
 
-console.log('Console listening!');
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('running'));
