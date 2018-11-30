d13e6147d2286a8ea62f9c7810902f143d4072fb
vosem <trefoil2@ukr.net>
2018-11-30 15:17:34 +0200

    'fix'

--- server.js
+++ server.js
@@ -4,14 +4,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '/dist/DinamicGallery')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + 'dist/index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/DinamicGallery/index.html'));
 });
 
-const port = process.env.PORT || 8080;
-app.set('port', port);
-
-const server = http.createServer(app);
-server.listen(port, () => console.log('running'));
+app.listen(process.env.PORT || 8080);
