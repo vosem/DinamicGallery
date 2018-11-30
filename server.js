ffda798b1b3048c551cf7502526506788a917867
vosem <trefoil2@ukr.net>
2018-11-30 14:08:51 +0200

    fixing push

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname = '/dist'));
-
+app.use(express.static(__dirname = 'dist'));
+app.engine('.html', require('ejs').__express);
+app.set('view engine', 'html');
 app.listen(process.env.PORT || 8080);
 
 app.get('/', function (req, res) {
-	res.render(path.join(__dirname = 'dist'));
+	res.render('index');
 });
 
 console.log('Console listening!');
