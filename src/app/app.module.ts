cc09b480c8e382a96ab12eca826ea491a8bf03a5
vosem <trefoil2@ukr.net>
2018-12-04 10:37:11 +0200

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CardComponent } from './card/card.component';
 import { ImageComponent } from './image/image.component';
 import { HoverDirective } from './hover.directive';
 
