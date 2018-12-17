6dd617ac08232e448c2193c7875ee7bf10117f1a
vosem <trefoil2@ukr.net>
2018-12-17 17:28:36 +0200

    testing mobile

--- src/app/hover.directive.ts
+++ src/app/hover.directive.ts
@@ -15,20 +15,20 @@ export class HoverDirective {
   @HostListener('touchend') onTouchEnd(e) {
     this.el.nativeElement.childNodes[0].classList.toggle('zoom');
     this.el.nativeElement.childNodes[1].classList.toggle('hoverActivated');
-    // this.el.nativeElement.childNodes[1].childNodes[1].classList.toggle('activated');
-    var a = this.el.nativeElement.childNodes[1].childNodes[1];
-    setTimeout( function(){
-      a.classList.add('activated');
-    }, 500);
-    setTimeout( function(){
-      a.classList.add('fadedIn');
-    }, 600);
-    setTimeout( function(){
-      a.classList.remove('fadedIn');
-    }, 3500);
-    setTimeout( function(){
-      a.classList.remove('activated');
-    }, 4000);
+    this.el.nativeElement.childNodes[1].childNodes[1].classList.toggle('activated');
+    // var a = this.el.nativeElement.childNodes[1].childNodes[1];
+    // var activatedOn = setTimeout( function(){
+    //   a.classList.add('activated');
+    // }, 500);
+    // var fadedInOn = setTimeout( function(){
+    //   a.classList.add('fadedIn');
+    // }, 600);
+    // var fadedInOut = setTimeout( function(){
+    //   a.classList.remove('fadedIn');
+    // }, 3500);
+    // var activatedOut = setTimeout( function(){
+    //   a.classList.remove('activated');
+    // }, 4000);
     e.preventDefault();
     e.stopPropagation();
   }
@@ -37,13 +37,18 @@ export class HoverDirective {
     console.log(this.el.nativeElement.childNodes);
     this.el.nativeElement.childNodes[0].classList.add('zoom');
     this.el.nativeElement.childNodes[1].classList.add('hoverActivated');
+    // this.el.nativeElement.childNodes[1].childNodes[1].classList.add('fadedIn');
     this.el.nativeElement.childNodes[1].childNodes[1].classList.add('activated');
+    // var a = this.el.nativeElement.childNodes[1].childNodes[1];
+    // setTimeout( function(){
+    //   a.classList.add('activated');
+    // }, 500);
   }
 
   @HostListener('mouseleave') onMouseLeave() {
     this.el.nativeElement.childNodes[0].classList.remove('zoom');
     this.el.nativeElement.childNodes[1].classList.remove('hoverActivated');
+    // this.el.nativeElement.childNodes[1].childNodes[1].classList.remove('fadedIn');
     this.el.nativeElement.childNodes[1].childNodes[1].classList.remove('activated');
   }
-
 }
