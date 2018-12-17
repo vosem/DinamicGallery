6f4203a47615a13c306338718b723e5d4655a953
vosem <trefoil2@ukr.net>
2018-12-17 14:47:18 +0200

    testing mobile

--- src/app/hover.directive.ts
+++ src/app/hover.directive.ts
@@ -15,7 +15,7 @@ export class HoverDirective {
   @HostListener('touchend') onTouchEnd(e) {
     this.el.nativeElement.childNodes[0].classList.toggle('zoom');
     this.el.nativeElement.childNodes[1].classList.toggle('hoverActivated');
-    this.el.nativeElement.childNodes[1].childNodes[1].classList.toggle('hidden');
+    // this.el.nativeElement.childNodes[1].childNodes[1].classList.toggle('activated');
     e.preventDefault();
     e.stopPropagation();
   }
@@ -24,11 +24,13 @@ export class HoverDirective {
     console.log(this.el.nativeElement.childNodes);
     this.el.nativeElement.childNodes[0].classList.add('zoom');
     this.el.nativeElement.childNodes[1].classList.add('hoverActivated');
+    this.el.nativeElement.childNodes[1].childNodes[1].classList.add('activated');
   }
 
   @HostListener('mouseleave') onMouseLeave() {
     this.el.nativeElement.childNodes[0].classList.remove('zoom');
     this.el.nativeElement.childNodes[1].classList.remove('hoverActivated');
+    this.el.nativeElement.childNodes[1].childNodes[1].classList.remove('activated');
   }
 
 }
